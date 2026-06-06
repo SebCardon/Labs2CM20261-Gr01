5d4b73a3bf7da5424c948c766bc2ea135bf640b4
Luis Gabriel <luis.alcalao@udea.edu.co>
2026-06-06 07:43:20 -0500

    corrección de algunos bugs y mejoramiento de UI

--- Lab2-Services/src/main/java/co/edu/udea/compumovil/gr01_20261/lab2/ui/screens/ChatScreen.kt
+++ Lab2-Services/src/main/java/co/edu/udea/compumovil/gr01_20261/lab2/ui/screens/ChatScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -62,7 +63,7 @@ fun ChatScreen(
                 navigationIcon = {
                     IconButton(onClick = onBackClick) {
                         Icon(
-                            imageVector = Icons.Default.ArrowBack,
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Atrás",
                             tint = MaterialTheme.colorScheme.onPrimary
                         )
